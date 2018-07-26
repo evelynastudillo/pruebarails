1abaac13c79461f016f5173da704d6844fb8d17a
evelyn <astudillobarra.evelyn@gmail.com>
2018-07-26 13:41:33 -0400

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  get 'todos', to: 'todos#index'
+  get '/todos', to: 'todos#index'
+  get 'todos/new'
+  post 'todos', to: 'todos#create'
+  get '/todos/:id', to: 'todos#show'
+  get '/todos/:id/edit', to: 'todos#edit'
+  patch '/todos/:id', to:'todos#update'
+  delete '/todos/:id', to: 'todos#destroy'
+  get '/todos/:id/complete', to: 'todos#complete'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
